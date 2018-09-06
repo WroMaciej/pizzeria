122c612440332f07ee47c38f99232b65cce5c896
WroMaciej <maciej.a.wroblewski@capgemini.com>
2018-09-06 10:52:15 +0200

    Parsing string to Category Enum works fine

--- src/app/collecting/collecting.component.ts
+++ src/app/collecting/collecting.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Category} from '../model/category.model';
+import { Category } from '../model/category.model';
+import { Router } from '@angular/router';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-collecting',
@@ -8,13 +10,31 @@ import {Category} from '../model/category.model';
 })
 export class CollectingComponent implements OnInit {
 
+
   category: Category;
 
-  
+  products: Array<Product>;
 
-  constructor() { }
+
+
+  constructor(private router: Router) {
+    this.category = this.urlToCategory(router.url);
+  }
 
   ngOnInit() {
+    console.log("Chosen category: " + this.category);
   }
+
+  populateProductList(){
+    
+  }
+
+  urlToCategory(url: string): Category {
+    let categoryName: string = url.slice(1, url.length);
+    categoryName = categoryName.charAt(0).toUpperCase() + categoryName.substr(1).toLowerCase();
+    return Category[categoryName];
+  }
+
+
 
 }
