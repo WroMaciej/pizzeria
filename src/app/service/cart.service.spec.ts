1334e7ce0afa4bfbf5cafb4345c7ef4ad4acc366
WroMaciej <maciej.a.wroblewski@capgemini.com>
2018-09-13 13:41:05 +0200

    Cart Service test mostly done

--- src/app/service/cart.service.spec.ts
+++ src/app/service/cart.service.spec.ts
@@ -17,13 +17,11 @@ describe('CartService test', () => {
         icon: 'icon.png'
     };
 
-
     beforeEach(() => {
         cartService = new CartService();
     });
 
-
-    it('should add product to empty cart',() => {
+    it('should add product to empty cart', () => {
         // GIVEN
         const productVariant: ProductVariant = {
             product: someProduct,
@@ -35,4 +33,59 @@ describe('CartService test', () => {
         expect(cartService.getProductQuantityNumber()).toBe(1);
         cartService.getTotalPrice().subscribe(totalPrice => expect(totalPrice).toBe(20));
     });
+
+    it('should group same product variants to one product quantity', () => {
+        // GIVEN
+        const productVariant: ProductVariant = {
+            product: someProduct,
+            size: 1
+        };
+        // WHEN
+        cartService.addProductVariant(productVariant);
+        cartService.addProductVariant(productVariant);
+        // THEN
+        expect(cartService.getProductQuantityNumber()).toBe(1);
+        cartService.getTotalPrice().subscribe(totalPrice => expect(totalPrice).toBe(40));
+    });
+
+    it('should calculate total price', () => {
+        // GIVEN
+        const productVariant1: ProductVariant = {
+            product: someProduct,
+            size: 1
+        };
+        const productVariant2: ProductVariant = {
+            product: someProduct,
+            size: 2
+        };
+        cartService.addProductVariant(productVariant1);
+        cartService.addProductVariant(productVariant1);
+        cartService.addProductVariant(productVariant2);
+        let totalPrice: number;
+        // WHEN
+        totalPrice = cartService.calculateTotalPrice();
+        // THEN
+        expect(totalPrice).toBe(70);
+    });
+
+    it('should clear cart', () => {
+        // GIVEN
+        const productVariant1: ProductVariant = {
+            product: someProduct,
+            size: 1
+        };
+        const productVariant2: ProductVariant = {
+            product: someProduct,
+            size: 2
+        };
+        cartService.addProductVariant(productVariant1);
+        cartService.addProductVariant(productVariant1);
+        cartService.addProductVariant(productVariant2);
+        // WHEN
+        cartService.clearCart();
+        // THEN
+        expect(cartService.calculateTotalPrice()).toBe(0);
+        expect(cartService.getProductQuantityNumber()).toBe(0);
+    });
+
 });
